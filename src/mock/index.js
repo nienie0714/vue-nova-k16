50957a5064a7ae40737dfc631cdf0a2162a1e51a
niexinyu <niexinyu0714@163.com>
2018-09-25 18:04:01 +0800

    repair

--- src/mock/index.js
+++ src/mock/index.js
@@ -1,4 +1,4 @@
 export default {
     url: require('./url'),
-    users1: require('./users1')
+    users: require('./user')
 };
--- src/mock/url.js
+++ src/mock/url.js
@@ -1,25 +1,47 @@
 import api from '../api/api';
 import Mock from 'mockjs';
 
-console.log(api.url.url);
 Mock.mock(RegExp(api.url.url + '*'), 'get', res => {
-    console.log(res.url.split('?')[1]);
-    if (res.url.split('?')[1] == 'RW=0&DevID=0&DP_Sta=0&HDMI_Sta=0&SDI1_Sta=0&SDI2_Sta=0&DVI1_Sta=0&DVI2_Sta=0&DVI3_Sta=0&DVI4_Sta=0&DVI_Mosaic_Sta=0&BKG_Sta=0&FRZ_Sta=0&BLACK_Sta=0&Account=0') {
-        return {
-            DP_Sta: '1',
-            HDMI_Sta: '2',
-            SDI1_Sta: '3',
-            SDI2_Sta: '0',
-            DVI1_Sta: '4',
-            DVI2_Sta: '0',
-            DVI3_Sta: '0',
-            DVI4_Sta: '0',
-            DVI_Mosaic_Sta: '0',
-            BKG_Sta: '0',
-            FRZ_Sta: '0',
-            BLACK_Sta: '0',
-            Account: '0',
-            ERRC: '0'
-        };
+    // 将参数截为数组
+    let params = new Object();
+    let str = res.url.split('?')[1];
+    let strs = str.split('&');
+    for (let i = 0; i < strs.length; i++) {
+        params[strs[i].split('=')[0]] = unescape(strs[i].split('=')[1]);
     }
+    for (let item in params) {
+        console.log(454545, item);
+    }
+    return Mock.mock({
+        'DP_Sta|0-4': 0,
+        'HDMI_Sta|0-4': 0,
+        'SDI1_Sta|0-4': 0,
+        'SDI2_Sta|0-4': 0,
+        'DVI1_Sta|0-4': 0,
+        'DVI2_Sta|0-4': 0,
+        'DVI3_Sta|0-4': 0,
+        'DVI4_Sta|0-4': 0,
+        'DVI_Mosaic_Sta|0-4': 0,
+        'BKG_Sta|0-4': 0,
+        'FRZ_Sta|0-4': 0,
+        'BLACK_Sta|0-4': 0,
+        'Account|0-4': 0,
+        ERRC: 0
+    });
+    // return {
+    //     DP_Sta: '1',
+    //     HDMI_Sta: '2',
+    //     SDI1_Sta: '3',
+    //     SDI2_Sta: '0',
+    //     DVI1_Sta: '4',
+    //     DVI2_Sta: '0',
+    //     DVI3_Sta: '0',
+    //     DVI4_Sta: '0',
+    //     DVI_Mosaic_Sta: '0',
+    //     BKG_Sta: '0',
+    //     FRZ_Sta: '0',
+    //     BLACK_Sta: '0',
+    //     Account: '0',
+    //     ERRC: '0'
+    // };
 });
