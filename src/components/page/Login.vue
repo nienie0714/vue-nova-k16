92a2e2ad503238af7af5adce79296de82b4f1198
home_wuhao <yy_wuhao@163.com>
2018-10-22 00:10:16 +0800

    login half

--- src/components/page/Login.vue
+++ src/components/page/Login.vue
@@ -1,18 +1,11 @@
 <template>
   <div class="login-wrap">
-    <div class="ms-title">后台管理系统</div>
+    <!-- <div class="ms-title">后台管理系统</div> -->
     <div class="ms-login">
-      <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="0px" class="demo-ruleForm">
-        <el-form-item prop="username">
-          <el-input v-model="ruleForm.username" placeholder="username"></el-input>
-        </el-form-item>
-        <el-form-item prop="password">
-          <el-input type="password" placeholder="password" v-model="ruleForm.password" @keyup.enter.native="submitForm('ruleForm')"></el-input>
-        </el-form-item>
+      <el-form>
         <div class="login-btn">
-          <el-button type="primary" @click="submitForm('ruleForm')">登录</el-button>
+          <el-button type="primary" @click="submitForm()">登录</el-button>
         </div>
-        <p style="font-size:12px;line-height:30px;color:#999;">Tips : 直接点击登录按钮即可登录。</p>
       </el-form>
     </div>
   </div>
@@ -24,18 +17,6 @@
   export default {
     data: function() {
       return {
-        ruleForm: {
-          username: '',
-          password: ''
-        },
-        rules: {
-          username: [
-            { required: false, message: '请输入用户名', trigger: 'blur' }
-          ],
-          password: [
-            { required: false, message: '请输入密码', trigger: 'blur' }
-          ]
-        }
       }
     },
     created() {
@@ -43,7 +24,7 @@
     methods: {
       ...mapMutations(['setMosic']),
       ...mapActions(['ajax']),
-      postLogin() {
+      submitForm() {
         let command = '';
         for(let i = 0; i <= 13; i++) {
           command += Math.floor(Math.random() * 10);
@@ -66,15 +47,6 @@
         }).then(res => {
           // { "In9_MosL":"1", "In9_MosM":"2", "In9_MosW":"1920", "In9_MosH":"1080", "ERRC": "0"}
           this.setMosic(res);
-        });
-      },
-      submitForm(formName) {
-        this.$refs[formName].validate((valid) => {
-          if(valid) {
-            this.postLogin();
-          } else {
-            return false;
-          }
         });
       }
     }
@@ -100,11 +72,10 @@
     position: absolute;
     left: 50%;
     top: 50%;
-    width: 300px;
-    height: 160px;
-    margin: -150px 0 0 -190px;
+    transform: translate(-50%, -50%);
+    width: 100%;
+    height: 300px;
     padding: 40px;
-    border-radius: 5px;
     background: #fff;
   }
   .login-btn {
